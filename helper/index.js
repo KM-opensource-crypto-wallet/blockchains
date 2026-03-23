0da1c90639b47bbad73b1aa0af47e5fcd2d58259
Swatantra Goswami <swatantragoswami@Swatantras-Mac-mini.local>
2026-03-23 19:53:36 +0530

    updated coderabit

--- helper/index.js
+++ helper/index.js
@@ -873,8 +873,8 @@ export const CustomRPCList = [
   {label: 'Arbitrum', value: 'arbitrum'},
   {label: 'Optimism', value: 'optimism'},
   {
-    label: 'Binance Smart Chain',
-    value: 'binance_smart_chain',
+    label: 'Optimism Binance Smart Chain',
+    value: 'optimism_binance_smart_chain',
   },
   {label: 'Avalanche', value: 'avalanche'},
   {label: 'Fantom', value: 'fantom'},
